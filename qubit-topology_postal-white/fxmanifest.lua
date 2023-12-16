b77db1c4ebc45548b0cce04f57649d617d274d65
Qubit <13704525+qubitrepos@users.noreply.github.com>
2023-12-16 14:19:01 +0300

    Purple, white postal codes added

---
+++ qubit-topology_postal-white/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+description "Qubit’s Topology In-Game Map"
+author "Qubit’s Topology In-Game Map"
+url "https://qubit.tebex.io"
+version "1.0"
+
+lua54 'yes'
