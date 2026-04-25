12a3b7d292124e8f55c2ab77082459ba24f6bced
Orhan Ozey <orhantechtm@gmail.com>
2026-04-25 16:43:34 +0300

    postal version removed, anti-alias added on textures, minimap visibility problem is fixed

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+client_scripts {
+	"client.lua",
+}
+
+description "Qubit’s Topology In-Game Map"
+author "Qubit’s Topology In-Game Map"
+url "https://qubit.tebex.io"
+version "1.0"
+
+lua54 'yes'
